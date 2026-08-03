e5d93e28e690ada54ce027109c4235cdfd3f97ff
RomanPR <prikhodjko.roman@gmail.com>
2026-08-04 00:18:41 +0300

    packages update, svg-icon fix

--- src/app/common-ui/svg-icon/svg-icon.component.ts
+++ src/app/common-ui/svg-icon/svg-icon.component.ts
@@ -19,5 +19,5 @@ export class SvgIconComponent {
     viewBox = input<string>('0 0 24 24');
     rotated = input<boolean>(false);
 
-    protected path = computed(() => `/assets/svgs/${this.name()}.svg#${this.name()}`);
+    protected path = computed(() => `assets/svgs/${this.name()}.svg#${this.name()}`);
 }
